61d0c1a4558f1b12ab2af816d50086b9ed51a606
gmelon <hsh1769@naver.com>
2023-04-03 23:43:16 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/dev/gmelon/plango/PlangoApplication.java
@@ -0,0 +1,13 @@
+package dev.gmelon.plango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gmelon/plango/PlangoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gmelon.plango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
