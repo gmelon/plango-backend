c5e4b01170c3d258dfb95eda86a9c0d1c3d8fd31
gmelon <hsh1769@naver.com>
2023-06-27 01:47:21 +0900

    chore: 불필요한 주석 삭제

--- src/main/java/dev/gmelon/plango/web/schedule/ScheduleController.java
+++ src/main/java/dev/gmelon/plango/web/schedule/ScheduleController.java
@@ -21,12 +21,8 @@ import java.util.List;
 @RestController
 public class ScheduleController {
 
-    // 개발 순서
-    // 컨트롤러 & DTO 개발 -> 서비스 컴파일 오류 해결 -> 컨트롤러 테스트 -> 서비스 작성 -> 서비스 테스트
-
     private final ScheduleService scheduleService;
 
-    // 스케쥴 생성
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void create(@LoginMember SessionMember sessionMember,
@@ -37,14 +33,12 @@ public class ScheduleController {
         response.addHeader(HttpHeaders.LOCATION, "/api/v1/schedules/" + scheduleId);
     }
 
-    // 스케쥴 단건 조회
     @GetMapping("/{scheduleId}")
     public ScheduleResponseDto findById(@LoginMember SessionMember sessionMember,
                                         @PathVariable Long scheduleId) {
         return scheduleService.findById(sessionMember.getId(), scheduleId);
     }
 
-    // 자신의 날짜별 스케쥴 리스트 조회
     @GetMapping("/day")
     public List<ScheduleListResponseDto> findAllByDate(
             @LoginMember SessionMember sessionMember,
@@ -52,7 +46,6 @@ public class ScheduleController {
         return scheduleService.findAllByDate(sessionMember.getId(), requestDate);
     }
 
-    // 월별 스케쥴이 존재하는 날짜들의 목록 조회
     @GetMapping("/month")
     public List<ScheduleCountResponseDto> getCountOfDaysInMonth(
             @LoginMember SessionMember sessionMember,
@@ -60,8 +53,6 @@ public class ScheduleController {
         return scheduleService.getCountOfDaysInMonth(sessionMember.getId(), requestMonth);
     }
 
-
-    // 스케쥴 수정 (PUT - 전체 정보가 서버로 보내진다고 가정)
     @PutMapping("/{scheduleId}")
     public void edit(@LoginMember SessionMember sessionMember,
                      @PathVariable Long scheduleId,
@@ -69,7 +60,6 @@ public class ScheduleController {
         scheduleService.edit(sessionMember.getId(), scheduleId, requestDto);
     }
 
-    // 스케쥴 삭제
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{scheduleId}")
     public void delete(@LoginMember SessionMember sessionMember,
