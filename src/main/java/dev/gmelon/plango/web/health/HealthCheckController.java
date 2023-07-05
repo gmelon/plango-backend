464930ed1a45454788da42fdaecafc24f96cb0a0
gmelon <hsh1769@naver.com>
2023-07-06 01:11:08 +0900

    fix: / 경로에 대한 로그인 여부 체크를 해제하고, /health와 동일한 값을 반환하도록 변경

--- src/main/java/dev/gmelon/plango/web/health/HealthCheckController.java
+++ src/main/java/dev/gmelon/plango/web/health/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/health")
+    @GetMapping({"/", "/health"})
     public String healthCheck() {
         return "healthy";
     }
