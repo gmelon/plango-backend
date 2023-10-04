5f4659edf1a00296b4cec915c7e91eda7f31c45a
gmelon <hsh1769@naver.com>
2023-10-04 17:39:58 +0900

    test: Service 계층의 중복 통합 테스트 제거

--- src/test/java/dev/gmelon/plango/service/notification/NotificationServiceTest.java
+++ src/test/java/dev/gmelon/plango/service/notification/NotificationServiceTest.java
@@ -6,9 +6,7 @@ import dev.gmelon.plango.domain.member.MemberRole;
 import dev.gmelon.plango.domain.notification.Notification;
 import dev.gmelon.plango.domain.notification.NotificationRepository;
 import dev.gmelon.plango.domain.notification.type.TestNotificationType;
-import dev.gmelon.plango.exception.notification.NotificationAccessDeniedException;
 import dev.gmelon.plango.infrastructure.fcm.FirebaseCloudMessageService;
-import dev.gmelon.plango.service.notification.dto.NotificationResponseDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.util.List;
-import java.util.stream.IntStream;
-
 import static dev.gmelon.plango.domain.notification.type.DefaultNotificationType.NotificationArguments;
-import static dev.gmelon.plango.domain.notification.type.DefaultNotificationType.SCHEDULE_INVITED;
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
 
 @Sql(value = "classpath:/reset.sql")
@@ -60,85 +52,6 @@ class NotificationServiceTest {
                 .role(MemberRole.ROLE_USER)
                 .build();
         memberRepository.save(memberB);
-    }
-
-    @Test
-    void 알림_목록을_페이징하여_조회한다() {
-        // given
-        List<Notification> notifications = IntStream.rangeClosed(0, 60)
-                .mapToObj(index -> Notification.builder()
-                        .title("알림 제목 " + index)
-                        .notificationType(SCHEDULE_INVITED)
-                        .member(memberA)
-                        .build())
-                .collect(toList());
-        notificationRepository.saveAll(notifications);
-
-        // when
-        List<NotificationResponseDto> responses = notificationService.findAll(memberA.getId(), 1);
-
-        // then
-        assertThat(responses).hasSize(40);
-        assertThat(responses.get(0).getTitle()).isEqualTo("알림 제목 60");
-        assertThat(responses.get(39).getTitle()).isEqualTo("알림 제목 21");
-    }
-
-    @Test
-    void 알림을_하나_삭제한다() {
-        // given
-        Notification givenNotification = Notification.builder()
-                .title("알림 제목")
-                .member(memberA)
-                .build();
-        notificationRepository.save(givenNotification);
-
-        // when
-        notificationService.deleteOne(memberA.getId(), givenNotification.getId());
-
-        // then
-        assertThat(notificationRepository.findById(givenNotification.getId())).isEmpty();
-    }
-
-    @Test
-    void 타인의_알림_삭제시_예외가_발생한다() {
-        // given
-        Notification givenNotification = Notification.builder()
-                .title("알림 제목")
-                .member(memberB)
-                .build();
-        notificationRepository.save(givenNotification);
-
-        // when, then
-        assertThatThrownBy(() -> notificationService.deleteOne(memberA.getId(), givenNotification.getId()))
-                .isInstanceOf(NotificationAccessDeniedException.class);
-        assertThat(notificationRepository.findById(givenNotification.getId())).isPresent();
-    }
-
-    @Test
-    void 알림을_모두_삭제한다() {
-        // given
-        List<Notification> givenNotifications = List.of(
-                Notification.builder()
-                        .title("알림 제목 1")
-                        .member(memberA)
-                        .build(),
-                Notification.builder()
-                        .title("알림 제목 2")
-                        .member(memberA)
-                        .build(),
-                Notification.builder()
-                        .title("알림 제목 3")
-                        .member(memberB)
-                        .build()
-        );
-        notificationRepository.saveAll(givenNotifications);
-
-        // when
-        notificationService.deleteAll(memberA.getId());
-
-        // then
-        assertThat(notificationRepository.findAll()).hasSize(1);
-        assertThat(notificationRepository.findAll().get(0).getMember().getId()).isEqualTo(memberB.getId());
     }
 
     @Test
