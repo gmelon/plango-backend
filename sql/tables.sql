0d79418d061db4e6189befc71897ea59ddacf06a
gmelon <hsh1769@naver.com>
2023-10-07 22:22:41 +0900

    chore: sql 테이블 DDL 추가, real 환경에서 실행 시 테이블 validation 하도록 옵션 변경

--- sql/tables.sql
+++ sql/tables.sql
@@ -13,6 +13,10 @@ create table schedule (id bigint not null auto_increment, created_time timestamp
 create table schedule_member (id bigint not null auto_increment, accepted BOOLEAN DEFAULT 0 not null, owner BOOLEAN DEFAULT 0 not null, member_id bigint, schedule_id bigint, primary key (id), foreign key (member_id) references member(id), foreign key (schedule_id) references schedule(id));
 create table place_search_record (id bigint not null auto_increment, keyword varchar(255) not null, last_searched_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null, member_id bigint, primary key (id), foreign key (member_id) references member(id));
 create table notification (id bigint not null auto_increment, created_time timestamp, modified_date timestamp, argument varchar(255), content varchar(255), notification_type varchar(255), title varchar(255), member_id bigint, primary key (id), foreign key (member_id) references member(id));
+create table firebase_cloud_message_token (id bigint not null auto_increment, last_updated_date timestamp not null, token_value varchar(255) not null, member_id bigint, primary key (id), foreign key (member_id) references member(id));
+create table schedule_place (id bigint not null auto_increment, created_time timestamp, modified_date timestamp, category varchar(10), confirmed BOOLEAN DEFAULT 0 not null, latitude double, longitude double, memo varchar(255), place_name varchar(255) not null, road_address varchar(255), schedule_id bigint,
+                        primary key (id), foreign key (schedule_id) references schedule(id));
+create table schedule_place_like (id bigint not null auto_increment, created_time timestamp, modified_date timestamp, member_id bigint, schedule_place_id bigint, primary key (id), foreign key (member_id) references member(id), foreign key (schedule_place_id) references schedule_place(id));
 
 # security
 create table persistent_logins (username varchar(64) not null, series varchar(64) primary key, token varchar(64) not null, last_used timestamp not null);
