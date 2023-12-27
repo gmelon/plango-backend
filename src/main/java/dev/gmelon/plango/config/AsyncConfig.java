966059e14a4184c61b380c0e6a84f6d81d2d6931
gmelon <hsh1769@naver.com>
2023-12-27 23:18:11 +0900

    fix: 테스트에서 @Async 가 적용되지 않도록 변경

--- src/main/java/dev/gmelon/plango/config/AsyncConfig.java
+++ src/main/java/dev/gmelon/plango/config/AsyncConfig.java
@@ -2,11 +2,13 @@ package dev.gmelon.plango.config;
 
 import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
+@Profile("!test")
 @Configuration(proxyBeanMethods = false)
 public class AsyncConfig implements AsyncConfigurer {
     private static final int ASYNC_THREAD_SIZE = 5;
